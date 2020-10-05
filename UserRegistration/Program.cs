89c64f277a232ef9199dcbbfd178bb72170e1eb0
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-06 00:01:23 +0530

    [mrunal] Add. Added feature to validate first name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
@@ -6,7 +7,27 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to User Registration Problem!");
+            Console.WriteLine("Welcome to User Registration Program!");
+            ValidateFirstName();
+        }
+
+        public static void ValidateFirstName()
+        {
+            bool valid = false;
+            while(!valid)
+            {
+                Console.Write("1. Enter First Name : ");
+                var firstName = Console.ReadLine();
+                Regex regex = new Regex("^([A-Z][a-z]*).{3,}$");
+
+                if (regex.IsMatch(firstName))
+                {
+                    Console.WriteLine("First Name is valid !");
+                    valid = true;
+                }
+                else
+                    Console.WriteLine("First Name is not valid !");
+            }
         }
     }
 }
