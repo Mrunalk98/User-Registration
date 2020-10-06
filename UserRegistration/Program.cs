fe043d15690c33ea1d6cff4e4a1105fde79a93d1
Kmrunal20 <mrunal.kurhade@digiplusit.com>
2020-10-06 09:22:28 +0530

    [mrunal] Refactor. Created UserValidation class and modified code for multiple input fields

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -5,48 +5,32 @@ namespace UserRegistration
 {
     class Program
     {
-        static string nameRegex = "^([A-Z][a-z]*).{3,}$";
+        static UserValidation validate = new UserValidation();
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Program!");
-            ValidateFirstName();
+            validate.ValidateName("First Name");
+            validate.ValidateName("Last Name");
+
         }
 
-        public static void ValidateFirstName()
-        {
-            bool valid = false;
-            while(!valid)
-            {
-                Console.Write("1. Enter First Name : ");
-                var firstName = Console.ReadLine();
-                Regex regex = new Regex(nameRegex);
 
-                if (regex.IsMatch(firstName))
-                {
-                    valid = true;
-                    ValidateLastName();
-                }
-                else
-                    Console.WriteLine("First Name is not valid ! \n");
-            }
-        }
+        //public static void ValidateLastName()
+        //{
+        //    bool valid = false;
+        //    while (!valid)
+        //    {
+        //        Console.Write("2. Enter Last Name : ");
+        //        var firstName = Console.ReadLine();
+        //        Regex regex = new Regex(nameRegex);
 
-        public static void ValidateLastName()
-        {
-            bool valid = false;
-            while (!valid)
-            {
-                Console.Write("2. Enter Last Name : ");
-                var firstName = Console.ReadLine();
-                Regex regex = new Regex(nameRegex);
-
-                if (regex.IsMatch(firstName))
-                {
-                    valid = true;
-                }
-                else
-                    Console.WriteLine("Last Name is not valid ! \n");
-            }
-        }
+        //        if (regex.IsMatch(firstName))
+        //        {
+        //            valid = true;
+        //        }
+        //        else
+        //            Console.WriteLine("Last Name is not valid ! \n");
+        //    }
+        //}
     }
 }
---
+++ UserRegistration/UserValidation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class UserValidation
+    {
+        string nameRegex = "^([A-Z][a-z]*).{2,}$";
+        
+        public void ValidateName(string text)
+        {
+            bool valid = false;
+            while (!valid)
+            {
+                Console.Write("Enter " + text + " : ");
+                var name = Console.ReadLine();
+                Regex regex = new Regex(nameRegex);
+                if (regex.IsMatch(name))
+                {
+                    valid = true;
+                }
+                else
+                    Console.WriteLine("Enter a valid " + text + " ! \n");
+            }
+        } 
+
+    }
+}
